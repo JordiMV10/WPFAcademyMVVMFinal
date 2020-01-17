5c3d0767eb54ec9d33b0b09ce8859f63fef8973c
JordiMV10 <jordimv10@hotmail.com>
2020-01-17 13:09:10 +0100

    Vista StudentExams a medias

---
+++ WPFAcademyMVVMFinal/ViewModels/StudentExamsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WPFAcademyMVVMFinal.Lib.UI;
+
+namespace WPFAcademyMVVMFinal.ViewModels
+{
+    public class StudentExamsViewModel : ViewModelBase
+    {
+
+    }
+}
